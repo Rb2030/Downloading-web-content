a11b2fe531ea318e22a9b4ebb543395958058ebe
Rb2030 <rossmail2050@gmail.com>
2017-05-19 10:24:56 +0100

    Initial commit

--- Downloading Web Content/ViewController.swift
+++ Downloading Web Content/ViewController.swift
@@ -10,11 +10,64 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var webview: UIWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+         //Do any additional setup after loading the view, typically from a nib.
 
+        
+//        let url = URL(string: "http://www.stackoverflow.com")!
+//        
+//        webview.loadRequest(URLRequest(url:url))
+// 
+// 
+//        
+//        webview.loadHTMLString("<h1>Hello there!</h1>", baseURL: nil)
+// 
+        
+        if let url = URL(string: "https://www.stackoverflow.com") { //This URL can be created from the url string
+        
+            let request = NSMutableURLRequest(url: url) //making a request to visit the website
+            
+            let task = URLSession.shared.dataTask(with: request as URLRequest) { //creating a task/session using the request that we used in the former line
+                
+                data, response, error in //checking to see if there is an error in the code below
+                
+                if error != nil {
+                
+                print(error!) // display an error
+            
+                }else {
+                    
+                    if let unwrappedData = data {
+                        
+                        let dataString = NSString(data: unwrappedData, encoding: String.Encoding.utf8.rawValue) // seeing if we can get any data variable, if we can, we get it from the utf8 data
+                        
+                        print(dataString)
+                        
+                        DispatchQueue.main.sync(execute: {
+                        
+                        //Update UI
+                        
+                        })
+
+                    }
+                    
+                }
+                
+                           }
+            
+            task.resume()
+        
+        }
+        
+        print("Hi There!")
+ 
+    }
+ 
+ 
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
